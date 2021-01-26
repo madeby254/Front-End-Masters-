5bb8cff8e31a126dbddfc6620b377c0f4df404ec
madeby254 <mojitsu185@gmail.com>
2021-01-25 22:23:50 -0800

    Oki doki

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let character = 'f';
 let timesToRepeat = 200;
 let answer = '';
 
-for (let i = 0; i < timesToRepeat; i++) {
+for (let i = 0; i <= timesToRepeat; i++) {
   answer += character;
 }
 
 console.log(answer);
+console.log(answer.length);
